fabb6e1a9a28aef14fa529d99809e25f3ca1717e
Alexander Wilkinson <alex.wilkinson@impala.travel>
2022-11-25 17:54:40 +0000

    working on main stylings

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import NavBar from "../src/components/NavBar";
 import styles from "../styles/Home.module.scss";
+import Header from "../src/components/Header";
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -17,6 +18,17 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <Header component="h1" variant="heading1">
+        {`Hello I'm Alex a frontend developer with full stack experience`}
+      </Header>
+      <p>
+        Located in Scotland I have experience with everything from Android
+        development to SCSS. I started in a more backend role with Java and C#
+        and spent two years working with React JS in media streaming. Now I am
+        working in a more full stack capacity on a Microsoft Stack with a front
+        end focus. In my freetime I enjoy playing with other technologies like
+        Rust and Electron and learning new and better ways to write code.
+      </p>
     </div>
   );
 }
