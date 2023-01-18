f74fb0e3cc570403f005617a1e635be45096e2d9
Alexander Wilkinson <alex.wilkinson@impala.travel>
2023-01-18 20:49:40 +0000

    feat: updated nav bar and bg next main content containers

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,16 +4,22 @@ import Text from "../../components/Text";
 const NavBar: React.FC = () => {
   return (
     <div className={styles.navBarContainer}>
-      <Text className={styles.navBarLogo} component="h1" variant="brand">
-        Alex W
-      </Text>
-      <ol className={styles.mainNav}>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>Blog</li>
-        <li>Resume</li>
-        <li>About</li>
-      </ol>
+      <div className={styles.brandContainer}>
+        <Text className={styles.navBarLogo} component="h1" variant="brand">
+          Alex W
+        </Text>
+      </div>
+      <nav className={styles.navBar}>
+        <ol className={styles.mainNav}>
+          <li>Blog</li>
+          <li>Portfolio</li>
+          <li>Resume</li>
+          <li>About</li>
+        </ol>
+      </nav>
+      <div className={styles.feedbackBtn}>
+        <Text component="span">Feedback</Text>
+      </div>
     </div>
   );
 };
--- src/components/_layout/StandardLayout.tsx
+++ src/components/_layout/StandardLayout.tsx
@@ -1,6 +1,8 @@
 import MainFooter from "../Footer/MainFooter";
 import NavBar from "../NavBar";
 import styles from "./StandardLayout.module.scss";
+import Image from "next/image";
+import BgImage from "../../../public/assets/background.png";
 
 interface Props {
   children: React.ReactNode;
@@ -9,6 +11,7 @@ interface Props {
 const StandardLayout: React.FC<Props> = ({ children }) => {
   return (
     <div className={styles.standardLayout}>
+      <Image className={styles.backgroundImage} alt="" src={BgImage} />
       <div>
         <NavBar />
       </div>
