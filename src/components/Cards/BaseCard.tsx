b7efebd6cc6711647f70f51375e26a65de4ad7cb
Alexander Wilkinson <alex.wilkinson@impala.travel>
2023-01-30 20:13:28 +0000

    chore: updated base card to with more style

--- src/components/Cards/BaseCard.tsx
+++ src/components/Cards/BaseCard.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import classnames from "classnames";
+
 import styles from "./BaseCard.module.scss";
 
 type Props = {
+  padding?: "none" | "small" | "medium" | "large";
   children: {
     header?: React.ReactNode;
     body: React.ReactNode;
@@ -9,8 +11,10 @@ type Props = {
   };
 };
 
-const BaseCard: React.FC<Props> = ({ children }) => (
-  <div className={styles.cardContainer}>
+const BaseCard: React.FC<Props> = ({ children, padding = "small" }) => (
+  <div
+    className={classnames(styles.cardContainer, styles[`padding-${padding}`])}
+  >
     {children.header && <div className={styles.header}>{children.header}</div>}
     <div className={styles.body}>{children.body}</div>
     {children.footer && <div className={styles.footer}>{children.footer}</div>}
