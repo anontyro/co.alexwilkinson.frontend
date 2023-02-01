bb7987515a114f7a78626e25456c1b7c3e5db89b
Alexander Wilkinson <alex.wilkinson@impala.travel>
2023-02-01 15:45:42 +0000

    feat: added resume top section next style the rest

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,18 +1,44 @@
-import FullWidthBox from "@/components/PageSections/FullWidthBox/FullWidthBox";
 import StandardLayout from "@/components/_layout/StandardLayout";
 import Text from "@/components/Text";
+import BoxWithImage from "@/components/PageSections/BoxWithImage/BoxWithImage";
+import MyPicture from "../../public/assets/my-picture.jpg";
+import GitHubIco from "@/components/Icons/github2.svg";
+import GlobeIco from "@/components/Icons/globe.svg";
 
 const PAGE_CONTENT = {
   TITLE: "my resume",
-  CONTENT: `Hi I’m Alex, a TypeScript developer with special focus on the frontend with React SCSS and an interest in UX`,
+  CONTENT: `Hi I’m Alex I’ve been working in software development since 2016 working on a range of different projects across the stack. I have experience with c# and JavaScript on both the frontend and backend. I specialise in Frontend development currently favouring React`,
+};
+
+const TopContent: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <GitHubIco />
+          /anontyro
+        </div>
+        <div>
+          <button>Resume</button>
+        </div>
+      </div>
+      <div>
+        <GlobeIco />
+        United Kingdom
+      </div>
+    </div>
+  );
 };
 
 const ResumePage: React.FC = () => {
   return (
     <StandardLayout>
-      <FullWidthBox title={PAGE_CONTENT.TITLE}>
-        <Text component="p">{PAGE_CONTENT.CONTENT}</Text>
-      </FullWidthBox>
+      <BoxWithImage image={MyPicture} title={PAGE_CONTENT.TITLE}>
+        {{
+          top: <TopContent />,
+          body: <Text component="p">{PAGE_CONTENT.CONTENT}</Text>,
+        }}
+      </BoxWithImage>
     </StandardLayout>
   );
 };
---
+++ src/components/PageSections/BoxWithImage/BoxWithImage.tsx
@@ -0,0 +1,38 @@
+import classnames from "classnames";
+import styles from "./BoxWithImage.module.scss";
+import Text from "@/components/Text";
+import Image, { StaticImageData } from "next/image";
+
+type Props = {
+  image: StaticImageData;
+  title: string;
+  children: {
+    top?: React.ReactNode;
+    body: React.ReactNode;
+  };
+};
+
+const BoxWithImage: React.FC<Props> = ({ image, title, children }) => (
+  <div className={styles.boxContainer}>
+    <div className={styles.backgroundBox}></div>
+    <div className={styles.boxImage}>
+      <Image src={image} alt={title} />
+    </div>
+    <div
+      className={classnames(
+        styles.boxContent,
+        children.top ? "" : styles.boxTopSpacer
+      )}
+    >
+      {children.top && (
+        <div className={styles.topContentContainer}>{children.top}</div>
+      )}
+      <Text className={styles.boxHeader} component="h2" variant="heading1">
+        {title}
+      </Text>
+      {children.body}
+    </div>
+  </div>
+);
+
+export default BoxWithImage;
